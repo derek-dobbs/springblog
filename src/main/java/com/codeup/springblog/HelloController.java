2bf4433dcfe608485b34310b02c35698266a331d
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-02-28 09:35:07 -0600

    Completed roll-dice

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -22,6 +23,29 @@ public class HelloController {
     @ResponseBody
     public String increment(@PathVariable int number) {
         return number + " plus one is " + (number + 1) + "!";
+    }
+
+    // Roll Dice from Views Exercise
+
+    @GetMapping("/roll-dice")
+    public String showOptions() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String showResult(@PathVariable int n, Model model) {
+        int randomNum = (int)(Math.random() * 6) + 1;
+
+//
+        model.addAttribute("randomNum", randomNum);
+//
+
+        if(n == randomNum) {
+            model.addAttribute("result", "You guessed correctly!");
+        } else {
+            model.addAttribute("result", "Sorry, your guess was incorrect.");
+        }
+        return "roll-dice";
     }
 
 }//end class HelloController
