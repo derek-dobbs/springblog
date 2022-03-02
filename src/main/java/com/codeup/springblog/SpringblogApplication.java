79b551d7635ce70f62baf5c93666e72c261cd73c
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-03-02 11:09:10 -0600

    Updated code

--- src/main/java/com/codeup/springblog/SpringblogApplication.java
+++ src/main/java/com/codeup/springblog/SpringblogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegulusSpringblogApplication {
+public class SpringblogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RegulusSpringblogApplication.class, args);
+        SpringApplication.run(SpringblogApplication.class, args);
     }
 
 }
