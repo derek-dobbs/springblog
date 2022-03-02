d9c72064a0c7398b9315ee374e290d9efab459a4
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-03-02 08:28:12 -0600

    Corrected code

--- src/main/java/com/codeup/springblog/repositories/AuthorRepository.java
+++ src/main/java/com/codeup/springblog/repositories/AuthorRepository.java
@@ -1,4 +1,4 @@
-package com.codeup.regulusspringblog.repositories;
+package com.codeup.springblog.repositories;
 
 import com.codeup.springblog.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
